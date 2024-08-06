0d02b7c8ad21e7667073b1afade648e92daef54e
Alex B. <105134673+albddnbn@users.noreply.github.com>
2024-08-06 11:41:22 -0400

    Making script execute when dot sourced
    Replace.ps1 script will execute when dot sourced by user - they can set the appname in the config, and rename the appname/deploy-appname.ps1 folder/file in one go.

--- replace.ps1
+++ replace.ps1
@@ -1,21 +1,28 @@
 # Run function to replace appname variable in config.ps1 file
-function Replace-AppName {
 
-    $config_ps1_file = Get-ChildItem -Path "." -Filter "config.ps1" -Recurse -File -ErrorAction SilentlyContinue
-    if (-not $config_ps1_file) {
-        Write-Host "config.ps1 file not found" -Foregroundcolor Red
-        return
-    }
 
-    $filepath = $config_ps1_file.FullName
-
-    $input = Read-Host "Enter appname value"
-
-    $content = Get-Content -Path $filePath
-
-    $updatedContent = $content -replace '\(\(\$appname\$\)\)', $input
-
-    $updatedContent | Set-Content -Path $filePath
-
-    Write-Host "Replaced (($appname$)) with $input in $filePath"
+$config_ps1_file = Get-ChildItem -Path "." -Filter "config.ps1" -Recurse -File -ErrorAction SilentlyContinue
+if (-not $config_ps1_file) {
+    Write-Host "config.ps1 file not found" -Foregroundcolor Red
+    return
 }
+
+$filepath = $config_ps1_file.FullName
+
+$input = Read-Host "Enter appname value"
+
+$content = Get-Content -Path $filePath
+
+$updatedContent = $content -replace '\(\(\$appname\$\)\)', $input
+
+$updatedContent | Set-Content -Path $filePath
+
+Write-Host "Replaced (($appname$)) with $input in $filePath"
+
+## Rename the Deploy-AppName.ps1 file:
+Rename-Item -Path ".\AppName\Deploy-AppName.ps1" -NewName "Deploy-$input.ps1" -Force
+
+## Rename the AppName directory:
+Rename-Item -Path ".\AppName" -NewName $input -Force
+
+
