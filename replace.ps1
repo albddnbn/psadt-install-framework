9095b07aff726c43039c217bd3b97af85882998d
Alex B. <105134673+albddnbn@users.noreply.github.com>
2024-08-06 18:34:45 -0400

    Update replace.ps1

--- replace.ps1
+++ replace.ps1
@@ -17,15 +17,6 @@ $updatedContent | Set-Content -Path $filePath
 
 Write-Host "Replaced (($appname$)) with $input in $filePath"
 
-## Create 'Files' directory for application files:
-New-Item -Path ".\AppName\Files" -ItemType Directory -Force | Out-Null
-
-## Rename the Deploy-AppName.ps1 file:
-Rename-Item -Path ".\AppName\Deploy-AppName.ps1" -NewName "Deploy-$input.ps1" -Force
-
-## Rename the AppName directory:
-Rename-Item -Path ".\AppName" -NewName $input -Force
-
 ## Change conflicting_processes in config.ps1 file:
 Write-host "For example, the applicaion executable for VS Code is code.exe"
 $process_name = Read-Host "Enter the application .exe / process name."
@@ -58,5 +49,15 @@ $content = Get-Content -Path $filePath
 $updatedContent = $content -replace '\(\(\$source_destination\$\)\)', $source_folder_dest
 
 $updatedContent | Set-Content -Path $filePath
+
+## Create 'Files' directory for application files:
+New-Item -Path ".\AppName\Files" -ItemType Directory -Force | Out-Null
+
+## Rename the Deploy-AppName.ps1 file:
+Rename-Item -Path ".\AppName\Deploy-AppName.ps1" -NewName "Deploy-$input.ps1" -Force
+
+## Rename the AppName directory:
+Rename-Item -Path ".\AppName" -NewName $input -Force
+
 
 
